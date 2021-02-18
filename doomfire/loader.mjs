559354a63c1d0f04e531516ef9c0b22b07f4c9d5
Anonymous <65428781+00ff0000red@users.noreply.github.com>
2021-02-17 16:05:21 -0800

    Create loader.mjs

---
+++ doomfire/loader.mjs
@@ -0,0 +1,60 @@
+// forked from https://github.com/binji/raw-wasm
+
+const {
+	document: {
+		head,
+		body
+	},
+	WebAssembly: {
+		Memory,
+		instantiateStreaming
+	},
+	Math,
+	globalThis,
+	Uint8ClampedArray,
+	ImageData
+} = self;
+
+head.querySelector("script").remove();
+
+const context = body.querySelector("canvas").getContext("2d");
+
+const memory = new Memory({
+	initial: 5,
+	maximum: 5,
+	shared: false
+});
+
+const data = new Uint8ClampedArray(
+	memory.buffer,
+	53760,
+	215040
+);
+
+const imageData = new ImageData(
+	data,
+	320,
+	168
+);
+
+instantiateStreaming(
+	fetch(
+		"./main.wasm", {
+			mode		: "same-origin",
+			credentials	: "omit",
+			cache		: "default",
+			referrer	: "no-referrer"
+		}
+	), [
+		Math,
+		globalThis, [
+			context.putImageData.bind(
+				context,
+				imageData,
+				0,
+				0
+			),
+			memory
+		]
+	]
+);
